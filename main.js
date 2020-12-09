0b35252c8f87b492c92bedd3054163cd6df232f1
jylee2 <leej1025y@live.com>
2020-12-09 12:52:29 +0800

    Plotting a bar chart

---
+++ main.js
@@ -0,0 +1,136 @@
+const jsonURL = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
+// Plot bar chart function
+const plotBarChart = (data, htmlElemId) => {
+   // svg canvas width set to 800px
+   const svgWidth = 800;
+   const svgHeight = 500;
+   const svgPadding = 40;
+   const barWidth = (svgWidth - 2*svgPadding) / data.length;
+
+   // **Draw canvas
+   const svgContainer = d3.select(htmlElemId)
+                           .append("svg")
+                           .attr("width", svgWidth)
+                           .attr("height", svgHeight)
+                           .attr("class", "row");
+
+   // **Generate scales
+   // Scale bar height
+   const barHeightScale = d3.scaleLinear()
+                              // Bar height values range from 0 GDP to max GDP
+                              .domain([0, d3.max(data, (d) => d[1])])
+                              // Output display range from 0 height to total svg height - 2 x padding
+                              .range([0, svgHeight - 2*svgPadding]);
+   // Scale bar x position
+   const barXPosScale = d3.scaleLinear()
+                           // Data index values range from 0th index to data.length - 1
+                           .domain([0, data.length - 1])
+                           // range() represents the start and end positions on the display for visualisation
+                           .range([svgPadding, svgWidth - svgPadding]);
+   // Convert date 'strings' in data[0] into numerical dates
+   const datesArray = data.map( (d) => {
+      return new Date(d[0]);
+   })
+   //console.log(datesArray);
+   // Scale x axis width, scaleTime because they are dates
+   const xAxisScale = d3.scaleTime()
+                     // Start from earliest date & end at latest date
+                     .domain([d3.min(datesArray), d3.max(datesArray)])
+                     // Display x axis starting from left to right
+                     .range([svgPadding, svgWidth - svgPadding]);
+   // Scale y axis height
+   const yAxisScale = d3.scaleLinear()
+                     // Start from 0 GDP & end at max GDP
+                     .domain([0, d3.max(data, (d) => d[1])])
+                     // Display y axis starting from bottom to top of svg, remember 0 = top of the page
+                     .range([svgHeight - svgPadding, svgPadding]);
+      
+   // **Generate axes
+   const xAxis = d3.axisBottom(xAxisScale);
+   //const xAxis = d3.axisBottom().scale(xAxisScale);
+   const xAxisTranslate = svgHeight - svgPadding; 
+   const yAxis = d3.axisLeft(yAxisScale);
+   // Create g element within svgContainer
+   const gSvgXAxis = svgContainer.append("g")
+                                 // Call xAxis
+                                 .call(xAxis)
+                                 .attr("id", "x-axis")
+                                 // Move x axis downwards, otherwise will be at top of the svg
+                                 .attr("transform", `translate(0, ${xAxisTranslate})`);
+   const gSvgYAxis = svgContainer.append("g")
+                                 .call(yAxis)
+                                 .attr("id", "y-axis")
+                                 .attr("transform", `translate(${svgPadding}, 0)`);
+
+   // **Add chart title
+   const title = svgContainer.append("text")
+                              .attr("id", "title")
+                              // Position in x from left of svg
+                              .attr("x", 280)
+                              // Position in y from top of svg
+                              .attr("y", svgPadding)
+                              .text("United States Gross Domestic Product");
+
+   // **Create rectangle svg shapes for the bar chart
+   const barChart = svgContainer.selectAll("rect")
+                                 // Put data into the waiting state for further processing
+                                 .data(data)
+                                 // Methods chained after data() run once per item in dataset
+                                 // Create new element for each piece of data
+                                 .enter()
+                                 // The following code will be ran for each data point
+                                 // Append rect for each data element
+                                 .append("rect")
+                                 // Add CSS class for hover effect
+                                 .attr("class", "bar")
+                                 // Set rect Bar width, not position
+                                 .attr("width", barWidth)
+                                 // Adding the requested "data-date" property into the <rect> element
+                                 .attr("data-date", (d, i) => d[0])
+                                 // Adding the requested "data-gdp" property into the <rect> element
+                                 .attr("data-gdp", (d, i) => d[1])
+                                 // Set x position of the bar from the left of the svg element
+                                 .attr("x", (d, i) => barXPosScale(i))
+                                 // Set rect Bar height, not position
+                                 .attr("height", (d, i) => barHeightScale(d[1]))
+                                 // Set y position of the bar from the top of the svg element
+                                 .attr("y", (d, i) => yAxisScale(d[1]))
+                                 // Fill bar with color
+                                 .attr("fill", "navy")
+                                 // Display data value when hover mouse on bar,
+                                 // but this doesn't pass the tests for some reason
+                                 .append("title")
+                                 .attr("id", "tooltip")
+                                 // Adding the requested "data-date" property into the <title> element
+                                 .attr("data-date", (d, i) => d[0])
+                                 // Specifying the text to display
+                                 .text((d) => `${d[0]}, $${d[1]} Billion`);
+
+};
+// Read freeCodeCamp JSON function
+const readJson = (jsonURL) => {
+   // Make a GET request to the URL
+   fetch(jsonURL)
+      // Take the response
+      .then( (response) => {
+         if (!response.ok) {
+            throw new Error(`HTTP error ${response.status}`);
+         }
+         // Convert the response to JSON format
+         return response.json();
+      })
+      // Handle the returned Promise by another .then() method which takes the JSON object as an argument
+      .then( (jsonObj) => {
+         // Note data[i][0] = date; data[i][1] = GDP;
+         const data = jsonObj["data"];
+         console.log("Data loaded.")
+         // Plot the bar chart by specifying (1) dataset & (2) which HTML Element Id to plot in
+         plotBarChart(data, "#insert-bar-chart");
+      })
+      // The catch block catches the error, and executes a code to handle it
+      .catch( (err) => {
+         return console.log(err);
+      })
+};
+// Execute all functions
+readJson(jsonURL);
